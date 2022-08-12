e4d396eff8bd10981fa30463a579007c2705c05c
KEISUKE KITAMURA <kitamuratcs@gmail.com>
2022-08-13 03:05:59 +0900

    Update sanitizer.js

--- common/sanitizer.js
+++ common/sanitizer.js
@@ -10,27 +10,41 @@ sanitizer.removeEmoji = () =>{
     target = sanitizer.text.replace(emojiRegex,"");
 
     return sanitizer;
-} ;
+};
+
+sanitizer.removeMention = () =>{
+    const regex_mention = /@+([a-zA-Z0-9亜-熙ぁ-んァ-ヶー-龥朗-鶴.\-_]+)/g;
+    target = sanitizer.text.replace(regex_mention,"");
+
+    return sanitizer;
+};
 
 sanitizer.removeHashtag = () =>{
     target = sanitizer.text.replace(/#/g,"");
 
     return sanitizer;
-}
+};
 
 sanitizer.removeLines = () =>{
     target = sanitizer.text.replace(/(\r\n|\n|\r)/gm,"");
 
     return sanitizer;
-}
+};
+
+sanitizer.removeFiles = () =>{
+    const regex_url = /(?:https?\:\/\/|www\.)[^\s]+/g;
+    target = sanitizer.text.replace(regex_url,"");
+
+    return sanitizer;
+};
 
 sanitizer.removeAll = (text) =>{
     target = text;
-    sanitizer.removeEmoji().removeHashtag().removeLines();
+    sanitizer.removeEmoji().removeMention().removeHashtag().removeLines().removeFiles();
     const sanitized = target;
     target = "";
     logger.debug(`(Sanitize Log): Target=${text} , Sanitized=${sanitized}`);
     return sanitized;
-}
+};
 
 module.exports = sanitizer;
