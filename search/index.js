f2d80b6554dc9476cd9c2422ff6c577acd6ce944
KEISUKE KITAMURA <kitamuratcs@gmail.com>
2022-08-13 16:45:59 +0900

    add search logic

--- search/index.js
+++ search/index.js
@@ -1,8 +1,18 @@
-// Todo: Add Some Logic
+const book = require("./book.js");
+const news = require("./news.js");
+
 const logic = {};
 
-logic.search = () =>{
+logic.search = async (keywords) =>{
+    const results = await Promise.all([
+        book.search(keywords),
+        news.search(keywords)
+    ]);
 
+    return {
+        book: results[0],
+        news: results[1]
+    };
 };
 
 module.exports = logic;
